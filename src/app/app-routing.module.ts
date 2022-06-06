373b35b86e3021da8a19d155b24a4dde05151f4d
daoson12 <daoduvictor48@gmail.com>
2022-06-06 18:56:06 +0100

    update app html added an image

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   {path: '', 
   component:HomeComponent},
-  { path: 'details', 
+  { path: 'details/:id', 
   loadChildren: () => import('./details/details.module')
   .then(m => m.DetailsModule) }
 ];
